5ffd4fa28c4be125d8c3861ca9c272a4deabf5a6
Ogurchick-fim <timlim.2006@gmail.com>
2024-12-12 18:07:51 +0600

    second-commit

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import "./index.css"
 
 export function Button(props){
-    const {variant = "primary",children} = props
-    const className = `my-buttin my-buttin--${variant}`
+    const {size="md",variant = "primary",children} = props
+    const className = `my-buttin my-buttin--${variant} my-buttin--${size}`
     return(
         <button className={className }>{children}</button>
     )
